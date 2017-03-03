d802eba7b94685803452c57e4fe097fa27e03619
Michael Simpson <msimpson@joinspartan.com>
2017-03-03 09:25:49 -0500

    Clean up code for `hex.pm`

--- lib/markovian.ex
+++ lib/markovian.ex
@@ -1,7 +1,20 @@
 defmodule Markovian do
+  @moduledoc """
+  Markovian is a Q-learner for the BEAM.
+  """
+
   defstruct alpha: 0.2, gamma: 0.9, q_table: %{}, current_state: nil, current_action: nil,
     random_action_rate: 0.4, random_action_decay: 0.99
 
+  @type t :: %__MODULE__{}
+  @type state :: term
+  @type action :: term
+
+  @doc """
+  Creates a new MDP. To build a new MDP, you need to provide a set of states
+  and actions as well an initial state.
+  """
+  @spec new(states::[state], actions::[action], initial_state::state) :: t
   def new(states, actions, initial_state) do
     state_space = for state <- states, into: %{} do
       action_space = for action <- actions, into: %{} do
@@ -11,9 +24,18 @@ defmodule Markovian do
       {state, action_space}
     end
 
-    %__MODULE__{q_table: state_space, current_state: initial_state, current_action: actions |> Enum.random()}
+    %__MODULE__{
+      q_table: state_space,
+      current_state: initial_state,
+      current_action: actions |> Enum.random()
+    }
   end
 
+  @doc """
+  Updates an MDP. It needs to get the reward and next state from the previous
+  state and action.
+  """
+  @spec update(t, float, state) :: t
   def update(mdp, reward, next_state) do
     s_update = (1 - mdp.alpha) * get_in(mdp.q_table, [mdp.current_state, mdp.current_action])
     next_q = mdp.q_table[next_state] |> Map.values() |> Enum.max()
@@ -25,13 +47,22 @@ defmodule Markovian do
   end
 
   defp generate_action(mdp) do
-    action = if mdp.random_action_rate > :rand.uniform do
-      mdp.q_table[mdp.current_state] |> Map.keys() |> Enum.random()
-    else
-      mdp.q_table[mdp.current_state] |> Map.to_list() |> Enum.max_by(&elem(&1, 1)) |> elem(0)
-    end
-
+    random? = mdp.random_action_rate > :rand.uniform
+    action = do_generate_action(mdp.q_table[mdp.current_state], random?)
     Map.put(mdp, :current_action, action)
+  end
+
+  defp do_generate_action(current_state_actions, random?)
+  defp do_generate_action(current_state_actions, false) do
+    current_state_actions
+    |> Map.to_list()
+    |> Enum.max_by(&elem(&1, 1))
+    |> elem(0)
+  end
+  defp do_generate_action(current_state_actions, true) do
+    current_state_actions
+    |> Map.keys()
+    |> Enum.random()
   end
 
   defp decay(mdp) do
--- mix.exs
+++ mix.exs
@@ -2,32 +2,41 @@ defmodule Markovian.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :markovian,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :markovian,
+      version: "0.1.1",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
+      package: package(),
+      dialyzer: [
+        plt_add_deps: :transitive,
+        flags: ["-Wunmatched_returns", "-Werror_handling", "-Wrace_conditions", "-Wunderspecs"],
+      ],
+      description: """
+      A Q-learner for the BEAM.
+      """,
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    # Specify extra applications you'll use from Erlang/Elixir
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp package do
+    [
+      maintainers: ["Michael Simpson"],
+      licenses: ["MPL-2.0"],
+      links: %{github: "https://github.com/mjs2600/Markovian"},
+     ]
+  end
+
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:credo, "~> 0.5", only: [:dev, :test]},
+    ]
   end
 end
